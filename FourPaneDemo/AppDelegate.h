3da206942338fc6ef7645d0effdbd974a66190ea
Алексей <alexey@macbook-pro-aleksej.local>
2016-03-16 21:29:27 +0300

    Initial Commit

---
+++ FourPaneDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FourPaneDemo
+//
+//  Created by Алексей on 16.03.16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FourPaneDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FourPaneDemo
+//
+//  Created by Алексей on 16.03.16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

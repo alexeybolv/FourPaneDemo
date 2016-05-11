16ff591be56aa71ae9764ccf9f2519fe6936183f
Алексей <alexey@macbook-pro-aleksej.local>
2016-05-12 00:44:32 +0300

    Before

--- FourPaneDemo/ViewController.m
+++ FourPaneDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "MapKit/MapKit.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -16,12 +21,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //WebView
+    NSString *webURL = @"https://www.facebook.com/westmontinspiredcomputinglab";
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:webURL]];
+    
+    [self.webView loadRequest:request];
+    
+    //Center the Map
+    double latitude = 34.448795;
+    double longitude = -119.6646337;
+    
+    MKPointAnnotation *wiclAnno = [[MKPointAnnotation alloc]init];
+    wiclAnno.coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    wiclAnno.title = @"WICL";
+    
+    [self.mapView addAnnotation:wiclAnno];
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(wiclAnno.coordinate, 10000, 10000);
+    
+    MKCoordinateRegion adjusted = [self.mapView regionThatFits:region];
+    
+    [self.mapView setRegion:adjusted animated:YES];
+    
+    // 
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
